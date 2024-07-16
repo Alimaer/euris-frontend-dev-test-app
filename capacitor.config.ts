7a472e6a74c8cad26fe8184a8ed95731cc1d2da2
Alimaer <sebastiano.darisi@gmail.com>
2024-07-16 14:25:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'euris-frontend-dev-test-app',
+  webDir: 'www'
+};
+
+export default config;

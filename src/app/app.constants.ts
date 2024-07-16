b2a08d2e6ef98b8bf38874da18cd917542f7251f
Alimaer <sebastiano.darisi@gmail.com>
2024-07-16 15:16:09 +0200

    constants, http injection

---
+++ src/app/app.constants.ts
@@ -0,0 +1,4 @@
+import { environment } from "src/environments/environment";
+
+export const BASE_URL = 'http://us-central1-test-b7665.cloudfunctions.net/api';
+export const STORE_ID = environment.storeId;

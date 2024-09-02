523bfd48b22abc10a902d772f021b37f2e329c4f
Alimaer <sebastiano.darisi@gmail.com>
2024-09-02 16:39:36 +0200

    store service added, services moved

---
+++ src/app/services/http/store.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BASE_URL, STORE_ID } from 'src/app/app.constants';
+import { IStore } from 'src/app/models/store.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  getStore(): Observable<IStore> {
+    return this.http.get<IStore>(
+      `${BASE_URL}/stores/${STORE_ID}`
+    );
+  }
+}

bd539892b6148a2a4158342f533a065074e06929
Alimaer <sebastiano.darisi@gmail.com>
2024-09-03 17:44:32 +0200

    product properties displayed

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,16 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { ProductsDataService } from '../services/products-data.service';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonRow, IonCol, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { IProduct } from '../models/product.model';
+import { ProductsDataService } from '../services/products-data.service';
+import { IProductData } from '../models/product-data.model';
+
+const emptySet: IProductData = {
+  reviews: [],
+  price: null,
+  description: null,
+  title: null,
+  category: null,
+  employee: null
+}
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonLabel
+  ]
 })
 export class ProductComponent  implements OnInit {
 
@@ -20,11 +44,16 @@ export class ProductComponent  implements OnInit {
 
   private productDataService = inject(ProductsDataService);
 
-  product: IProduct | null = null;
+  private product: IProduct | null = null;
 
   constructor() { }
 
+  get data() {
+    return this.product?.data ?? emptySet;
+  }
+
   ngOnInit() {
+    console.log(this.product);
   }
 
 }

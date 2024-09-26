2e1a60aaa09cf044b66aef19ef6d7363ccfc1265
Alimaer <sebastiano.darisi@gmail.com>
2024-09-26 17:18:36 +0200

    creation component wired

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,16 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import * as Ion from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add, close } from 'ionicons/icons';
 import { delay, finalize } from 'rxjs';
 import { IProductList } from '../models/product-list.model';
 import { IProduct } from '../models/product.model';
 import { IStore } from '../models/store.model';
+import { ProductCreateComponent } from '../product-create/product-create.component';
 import { ProductComponent } from '../product/product.component';
 import { ProductsCallerService } from '../services/http/products-caller.service';
 import { StoreCallerService } from '../services/http/store-caller.service';
 import { ProductsDataService } from '../services/products-data.service';
-import { addIcons } from 'ionicons';
-import { add, close } from 'ionicons/icons';
-import { ProductCreateComponent } from '../product-create/product-create.component';
-import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -35,15 +34,11 @@ import { OverlayEventDetail } from '@ionic/core/components';
     Ion.IonButton,
     Ion.IonButtons,
     Ion.IonIcon,
-    Ion.IonModal,
-    Ion.IonFooter,
     ProductComponent,
     ProductCreateComponent
   ],
 })
 export class HomePage {
-
-  @ViewChild(Ion.IonModal) modal: Ion.IonModal | undefined;
 
   dummyProducts = new Array(5);
   products: IProduct[] = [];
@@ -51,11 +46,10 @@ export class HomePage {
   isProductsLoading = false;
   isStoreLoading = false;
   error: string | null = null;
-  name = 'productCreationModal';
 
   private currentPage = 1;
   private productService = inject(ProductsCallerService);
-  private productDataService  = inject(ProductsDataService);
+  private productDataService = inject(ProductsDataService);
   private storeService = inject(StoreCallerService);
 
   constructor() {
@@ -96,7 +90,7 @@ export class HomePage {
   }
 
   loadMoreProducts(event: Ion.InfiniteScrollCustomEvent) {
-    if(this.products.length < 10) {
+    if (this.products.length < 10) {
       event.target.complete();
       return;
     }
@@ -128,22 +122,5 @@ export class HomePage {
         this.error = error;
       }
     });
-  }
-
-  cancel() {
-    if(this.modal)
-      this.modal.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    if(this.modal)
-      this.modal.dismiss(this.name, 'confirm');
-  }
-
-  onWillDismiss(event: Event) {
-    const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    if (ev.detail.role === 'confirm') {
-      console.log('ciao');
-    }
   }
 }
--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonCol, IonGrid, IonIcon, IonInput, IonItem, IonLabel, IonList, IonRow, IonButton } from '@ionic/angular/standalone';
+import * as Ion from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { add, close } from 'ionicons/icons';
+import * as _ from 'lodash';
+import { IProduct } from '../models/product.model';
+import { ProductsCallerService } from '../services/http/products-caller.service';
+
 
 @Component({
   selector: 'app-product-create',
@@ -14,27 +18,41 @@ import { add, close } from 'ionicons/icons';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonItem,
-    IonLabel,
-    IonList,
-    IonInput,
-    IonIcon,
-    IonButton
+    Ion.IonGrid,
+    Ion.IonRow,
+    Ion.IonCol,
+    Ion.IonItem,
+    Ion.IonLabel,
+    Ion.IonList,
+    Ion.IonInput,
+    Ion.IonIcon,
+    Ion.IonButton,
+    Ion.IonModal,
+    Ion.IonFooter,
+    Ion.IonButtons,
+    Ion.IonToolbar,
+    Ion.IonContent,
+    Ion.IonHeader
   ]
 })
 export class ProductCreateComponent {
 
+  @ViewChild(Ion.IonModal) modal: Ion.IonModal | undefined;
+
   private builder = inject(FormBuilder);
+  private productService = inject(ProductsCallerService);
+
+  name = 'productCreationModal';
+  error: string | null = null;
+  isLoading: boolean = false;
 
   newProductGroup = this.builder.group({
-    descrizione: null,
-    categoria: null,
-    impiegato: null,
-    prezzo: null,
-    recensioni: this.builder.array([
+    description: null,
+    category: null,
+    employee: null,
+    price: null,
+    title: null,
+    reviews: this.builder.array([
       this.builder.control('')
     ])
   });
@@ -44,7 +62,7 @@ export class ProductCreateComponent {
   }
 
   get recensioni() {
-    return this.newProductGroup.get('recensioni') as FormArray;
+    return this.newProductGroup.get('reviews') as FormArray;
   }
 
   addReview() {
@@ -53,6 +71,35 @@ export class ProductCreateComponent {
 
   removeReview(i: number) {
     this.recensioni.removeAt(i);
+  }
+
+  cancel() {
+    if (this.modal)
+      this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.isLoading = true;
+
+    const newProduct: IProduct = {
+      id: null,
+      data: {
+        ...this.newProductGroup.getRawValue(),
+        reviews: _.compact(this.newProductGroup.get('reviews')?.value ?? [])
+      }
+    };
+
+    this.productService.addProduct(newProduct).subscribe({
+      next: () => {
+        this.isLoading = false;
+        if (this.modal)
+          this.modal.dismiss(this.name, 'confirm');
+      },
+      error: error => {
+        this.error = error;
+        this.isLoading = false;
+      }
+    });
   }
 
 }
